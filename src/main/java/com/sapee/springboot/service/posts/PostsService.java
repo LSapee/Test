0388f6a7d038472130d3f6421e06378f40d2c95a
Lsapee <dlworhks94@gmail.com>
2021-12-10 22:49:16 +0900

    4장 오류 수정해야함

--- src/main/java/com/sapee/springboot/service/posts/PostsService.java
+++ src/main/java/com/sapee/springboot/service/posts/PostsService.java
@@ -3,12 +3,16 @@ package com.sapee.springboot.service.posts;
 
 import com.sapee.springboot.domain.post.Posts;
 import com.sapee.springboot.domain.post.PostsRepository;
+import com.sapee.springboot.web.dto.PostsListResponseDto;
 import com.sapee.springboot.web.dto.PostsSaveRequestDto;
 import com.sapee.springboot.web.dto.PostsResponseDto;
 import com.sapee.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -39,4 +43,19 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->
+                        new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
+    }
 }
--- src/main/java/com/sapee/springboot/web/IndexController.java
+++ src/main/java/com/sapee/springboot/web/IndexController.java
@@ -1,17 +1,29 @@
 package com.sapee.springboot.web;
 
+import com.sapee.springboot.service.posts.PostsService;
+import com.sapee.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts",dto);
 
-    @GetMapping("posts/save")
-    public String postsSave(){
-        return "posts-save";
+        return "posts-update";
     }
 }
