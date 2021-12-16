17935c622f9a5a78d3637c53ba174e0d1129cd1c
Lsapee <dlworhks94@gmail.com>
2021-12-16 20:50:11 +0900

    google OAUth 네이버 로그인시 에러, 구글 로그인시 이름대신 user로 에러 수정 및 EC2 세팅중

--- src/main/java/com/sapee/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/sapee/springboot/config/auth/dto/OAuthAttributes.java
@@ -47,8 +47,8 @@ public class OAuthAttributes {
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
         return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
+                .name((String)attributes.get("name"))
+                .email((String)attributes.get("email"))
                 .picture((String)attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
@@ -60,8 +60,8 @@ public class OAuthAttributes {
         Map<String, Object> response =(Map<String, Object>) attributes.get("response");
 
         return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
+                .name((String)attributes.get("name"))
+                .email((String)attributes.get("email"))
                 .picture((String)attributes.get("profile_image"))
                 .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
