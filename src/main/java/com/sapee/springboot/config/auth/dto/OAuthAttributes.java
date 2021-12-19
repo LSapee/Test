ed2d47f63efe3acd7a225f8d7aeb241bb4bb2767
Lsapee <dlworhks94@gmail.com>
2021-12-19 22:50:05 +0900

    네이버 /login으로 가지는 오류는 수정 완료 네이버 및 구글 로그인시 user로 나오는 오류 수정해야함

--- src/main/java/com/sapee/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/sapee/springboot/config/auth/dto/OAuthAttributes.java
@@ -16,7 +16,7 @@ public class OAuthAttributes {
     private String picture;
 
     @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture){
+    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
@@ -24,18 +24,37 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    public static OAuthAttributes of(String registrationId,
-                                     String userNameAttributeName,
-                                     Map<String, Object> attributes){
-
-            if("naver".equals(registrationId)){
-                return ofNaver("id",attributes);
-            }
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
 
         return ofGoogle(userNameAttributeName, attributes);
     }
 
-    public User toEntity(){
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public User toEntity() {
         return User.builder()
                 .name(name)
                 .email(email)
@@ -43,31 +62,4 @@ public class OAuthAttributes {
                 .role(Role.GUEST)
                 .build();
     }
-
-
-    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
-        return OAuthAttributes.builder()
-                .name((String)attributes.get("name"))
-                .email((String)attributes.get("email"))
-                .picture((String)attributes.get("picture"))
-                .attributes(attributes)
-                .nameAttributeKey(userNameAttributeName)
-                .build();
-    }
-
-//    Naver
-    private static OAuthAttributes ofNaver(String userNameAttributeName,Map<String, Object> attributes ){
-        Map<String, Object> response =(Map<String, Object>) attributes.get("response");
-
-        return OAuthAttributes.builder()
-                .name((String)attributes.get("name"))
-                .email((String)attributes.get("email"))
-                .picture((String)attributes.get("profile_image"))
-                .attributes(response)
-                .nameAttributeKey(userNameAttributeName)
-                .build();
-    }
-
-
-
 }
--- src/main/java/com/sapee/springboot/web/IndexController.java
+++ src/main/java/com/sapee/springboot/web/IndexController.java
@@ -14,28 +14,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
-
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model, @LoginUser SessionUser user){
-        model.addAttribute("posts",postsService.findAllDesc());
-
-        if(user != null){
-            model.addAttribute("userName",user.getName());
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
         }
-
         return "index";
     }
+
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
+    public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post",dto);
+        model.addAttribute("post", dto);
 
         return "posts-update";
     }
