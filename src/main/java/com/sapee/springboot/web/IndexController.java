c2095b19acfa99361ad0f472191f1a023ada33cd
Lsapee <dlworhks94@gmail.com>
2021-12-20 16:19:34 +0900

    오류 조금 해결

--- src/main/java/com/sapee/springboot/web/IndexController.java
+++ src/main/java/com/sapee/springboot/web/IndexController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+
     private final PostsService postsService;
 
     @GetMapping("/")
--- src/test/java/com/sapee/springboot/web/HelloControllerTest.java
+++ src/test/java/com/sapee/springboot/web/HelloControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,16 +19,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebAppConfiguration
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = HelloController.class,
-        excludeFilters = {
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
-})
 
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello() throws Exception{
         String hello = "hello";
@@ -36,6 +34,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception{
         String name = "hello";
